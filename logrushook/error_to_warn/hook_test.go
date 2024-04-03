317a6c16a94283b9570bb0b1f67a3e8d6606dd44
liangjunmo <liangjunmo@foxmail.com>
2024-04-03 19:05:27 +0800

    logrushook: refactor tests

--- logrushook/error_to_warn/hook_test.go
+++ logrushook/error_to_warn/hook_test.go
@@ -13,11 +13,11 @@ import (
 
 func TestHook_TransformToWarn(t *testing.T) {
 	var (
+		buffer   bytes.Buffer
+		fields   logrus.Fields
 		notFound code.Code = "NotFound"
 		key                = "level"
 		value              = "warning"
-		buffer   bytes.Buffer
-		fields   logrus.Fields
 	)
 
 	hook := NewHook(nil, true)
@@ -28,6 +28,28 @@ func TestHook_TransformToWarn(t *testing.T) {
 	log.AddHook(hook)
 
 	log.WithError(notFound).Error("message")
+	err := json.Unmarshal(buffer.Bytes(), &fields)
+	require.Nil(t, err)
+	require.Equal(t, value, fields[key])
+}
+
+func TestHook_NotTransformToWarn(t *testing.T) {
+	var (
+		buffer              bytes.Buffer
+		fields              logrus.Fields
+		internalServerError code.Code = "InternalServerError"
+		key                           = "level"
+		value                         = "error"
+	)
+
+	hook := NewHook([]code.Code{internalServerError}, true)
+
+	log := logrus.New()
+	log.SetFormatter(&logrus.JSONFormatter{})
+	log.SetOutput(&buffer)
+	log.AddHook(hook)
+
+	log.WithError(internalServerError).Error("message")
 	err := json.Unmarshal(buffer.Bytes(), &fields)
 	require.Nil(t, err)
 	require.Equal(t, value, fields[key])
